c7a52fb25965ffd918990cc162cdb8481e45e335
yellow-jellyfish <90250541+yellow-jellyfish@users.noreply.github.com>
2021-11-15 16:47:18 +0100

    The message bc reasons

--- ingest/app.py
+++ ingest/app.py
@@ -1,10 +1,9 @@
 import requests
 import re
-#import google.cloud
-#from google.cloud import storage
+from google.cloud import storage
 import os
 
-#storage_client = storage.Client()
+storage_client = storage.Client()
 
 def get_covid19_cases():
     """Download covid-19-world cases data from the aws repository. If fails to download, returns None."""
@@ -17,37 +16,24 @@ def get_covid19_cases():
         fileref.write(resp.content)
     return resp.status 
 
-get_covid19_cases()
 
-
-def upload_to_bucket(path_to_file, bucket_name):
-    """Upload a file from `path_to_file` to desired `bucket_name`.
-
-    :param path_to_file: The path to the file on the host machine (local system or server).
-    :param bucket_name: The target storage blob bucket name.
-    """
-    blob_name = os.path.basename(path_to_file)
+def upload_to_bucket(file_name, bucket_name):
+    """Upload a file to desired `bucket_name`"""
+    blob_name = file_name
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(blob_name)
-    blob.upload_from_filename(path_to_file)
+    blob.upload_from_filename("covid-19-world-cases.csv")
 
     # returns a public url
     return blob.public_url
 
 
 def main():
-    dates = get_available_dailies()
-    bucket_name = "covid19_twitter"
-    for idx, date in enumerate(dates):
-        print(f"{idx}: downloading {date}")
-        download_path = get_covid19_twitter(date)
-        if not download_path:
-            continue
-        print(f"{idx}: uploading {date}")
-        upload_to_bucket(download_path, bucket_name)
-        print(f"{idx}: removing {date}")
-        os.remove(download_path)
+    bucket_name = "covid19_cases"
+    file_name = "covid-19-world-cases.csv"
+    get_covid19_cases()
+    print("Downloaded covid cases data")
+    upload_to_bucket(file_name, bucket_name)
 
-
-#if __name__ == "__main__":
-#    main()
+if __name__ == "__main__":
+    main()
